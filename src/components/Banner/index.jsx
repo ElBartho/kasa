0beb234fed00378ecb00526a9f8babb95f8b0581
Jérémy Barthelemy <barthelemy.pro@gmail.com>
2023-11-28 20:42:59 +0100

    update banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,15 +1,11 @@
 import { Box, BoxTitle } from './banner';
 
-function Banner({ title, background }) {
+function Banner({ title = '', background }) {
   return (
     <Box background={background}>
       <BoxTitle>{title}</BoxTitle>
     </Box>
   );
 }
-
-Banner.defaultProps = {
-  title: '',
-};
 
 export default Banner;
